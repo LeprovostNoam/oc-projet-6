34e8126c766718e6f0bf530f1ecdeff5c5fd9e12
OpenClassrooms <noam@leprovost.pro>
2024-06-03 17:15:30 +0200

    Ajout de la page 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
+import '../assets/css/NotFound.scss';
+
 function NotFound() {
   return (
     <HelmetProvider>
       <div>
         <Helmet>
           <title>Kasa FR | 404</title>
+          <meta name="robots" content="noindex" />
         </Helmet>
-        <h1>404 Not Found</h1>
+        <section className="not-found">
+          <h1>404</h1>
+          <p>Oups! La page que vous demandez n'existe pas.</p>
+          <a href="/">Retourner sur la page d'accueil</a>
+        </section>
       </div>
     </HelmetProvider>
   );
