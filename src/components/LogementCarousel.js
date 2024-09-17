62137f74d4d43add289dbf711e90530e7ca6e79c
OpenClassrooms <noam@leprovost.pro>
2024-09-17 18:35:03 +0200

    Mise à jour de la page logement 2/2

--- src/components/LogementCarousel.js
+++ src/components/LogementCarousel.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import chevronRight from '../assets/img/chevron-right.svg';
 import chevronLeft from '../assets/img/chevron-left.svg';
 import '../assets/css/LogementCarousel.scss';
@@ -9,56 +7,44 @@ const LogementCarousel = ({ pictures }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const handlePrevClick = () => {
-    if (currentSlide === 0) {
-      setCurrentSlide(pictures.length - 1);
-    } else {
-      setCurrentSlide(currentSlide - 1);
-    }
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? pictures.length - 1 : prevSlide - 1));
   };
 
   const handleNextClick = () => {
-    if (currentSlide === pictures.length - 1) {
-      setCurrentSlide(0);
-    } else {
-      setCurrentSlide(currentSlide + 1);
-    }
+    setCurrentSlide((prevSlide) => (prevSlide === pictures.length - 1 ? 0 : prevSlide + 1));
   };
 
   const showArrowsAndPagination = pictures.length > 1;
 
   return (
     <div className="logement-carousel">
-      <Carousel
-        showThumbs={false}
-        dynamicHeight={true}
-        showStatus={false}
-        renderIndicator={() => null}
-        selectedItem={currentSlide}
-        onChange={(index) => setCurrentSlide(index)}
-        renderArrowPrev={(onClickHandler, hasPrev, label) =>
-          showArrowsAndPagination && (
-            <button type="button" onClick={handlePrevClick} title={label} className="arrow arrow-prev">
-              <img src={chevronLeft} alt="Précédent" />
-            </button>
-          )
-        }
-        renderArrowNext={(onClickHandler, hasNext, label) =>
-          showArrowsAndPagination && (
-            <button type="button" onClick={handleNextClick} title={label} className="arrow arrow-next">
-              <img src={chevronRight} alt="Suivant" />
-            </button>
-          )
-        }
-      >
-        {pictures.map((picture, index) => (
-          <div key={index}>
-            <img src={picture} alt={`Logement ${index + 1}`} />
-          </div>
-        ))}
-      </Carousel>
+      {showArrowsAndPagination && (
+        <button
+          type="button"
+          onClick={handlePrevClick}
+          className="arrow arrow-prev"
+        >
+          <img src={chevronLeft} alt="Précédent" />
+        </button>
+      )}
+
+      <div className="carousel-slide">
+        <img src={pictures[currentSlide]} alt={`Logement ${currentSlide + 1}`} className="carousel-image" />
+      </div>
+
+      {showArrowsAndPagination && (
+        <button
+          type="button"
+          onClick={handleNextClick}
+          className="arrow arrow-next"
+        >
+          <img src={chevronRight} alt="Suivant" />
+        </button>
+      )}
+
       {showArrowsAndPagination && (
         <div className="pagination">
-          {currentSlide + 1}/{pictures.length}
+          {currentSlide + 1} / {pictures.length}
         </div>
       )}
     </div>
